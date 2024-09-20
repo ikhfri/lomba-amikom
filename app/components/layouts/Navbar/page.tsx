59db363d8945004f143fa2ab221ca8437fb0d2a2
alanabyan <alanabyan4@gmail.com>
2024-09-20 11:13:52 +0700

    Update Navbar and Hover Gif

--- app/components/layouts/Navbar/page.tsx
+++ app/components/layouts/Navbar/page.tsx
@@ -1,17 +1,36 @@
+'use client'
+
+import { useState } from "react";
 import navbarData from "@/app/data/ListNavbar/DataListNavbar.json";
 import Link from "next/link";
 
 export default function Navbar() {
-  const navbarItems = navbarData.navbar;
+  const navbarItems = navbarData.navbar || [];
+  const [hoveredIndex, setHoveredIndex] = useState<number | string | null>(null)
+  
+  console.log(navbarItems)
 
   return (
     <main className="flex justify-center">
       <nav className="w-full h-20 bg-white text-[#969696dd] font-semibold">
         <ul className="flex justify-end mt-7 mr-7 text-center">
             {navbarItems.map((item, index) => (
-                <Link key={index} href={item.link}>
-                    <li className="px-2">{item.label}</li>
-                </Link>
+               <li
+               key={index}
+               className="flex items-center"
+               
+             >
+               <Link href={item.link} className="flex items-center">
+                 <img
+                  onMouseEnter={() => setHoveredIndex(item.static)}
+                  onMouseLeave={() => setHoveredIndex(null)}
+                   src={hoveredIndex === item.static ? item.gif : item.static}
+                   className={`h-5 z-10 ${hoveredIndex ? 'h-12' : ''}`}
+                   alt={item.label}
+                 />
+                 <span className="px-2">{item.label}</span>
+               </Link>
+             </li>
             ))}
         </ul>
       </nav>
--- app/pages/Hero/page.tsx
+++ app/pages/Hero/page.tsx
@@ -9,22 +9,8 @@ const robotoSlab = Roboto_Slab({
 })
 
 export default function HeroPage() {
-    const videoRef = useRef(null);
 
     return (
-        <div className={` w-full relative`}>
-            <video autoPlay loop muted className="h-fit opacity-[0.5]" ><source src="/flower.mp4" type="video/mp4" /></video>
-            <h1 className={`${robotoSlab.className} shadow-[0px_20px_20px_10px_#00000024] left-[36%] top-[40%] absolute text-[8vh]`}>TechnoTani</h1>
-            {/* <div className="flex items-center justify-center absolute bottom-5 left-1/2 translate-x-[-50%] w-16 h-16 bg-[rgba(255, 255, 255, 0.8)] rounded-[50%]"></div> */}
-            <video  ref={videoRef}
-            className="w-[5%] h-auto"
-            loop
-            muted
-            onMouseEnter={() => videoRef.current.play()}
-            onMouseLeave={() => {
-                videoRef.current.pause();
-                videoRef.current.currentTime = 0; // Reset ke awal
-            }}><source src="/shopping-cart.mp4" type="video/mp4" /></video>
-        </div>
+       <div className=""></div>
     )
 }
