61fb59a913ffd09157c5303d6621a5f2172b6d51
ikhfri <ikhsanfahrigultom@gmail.com>
2024-09-30 14:43:36 +0700

    add data testimoni and avatar image

--- app/components/layouts/Navbar/page.tsx
+++ app/components/layouts/Navbar/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import { useState } from "react";
@@ -14,24 +15,24 @@ export default function Navbar() {
 
   return (
     <main className="flex justify-center">
-      <nav className="w-[40vw] rounded-full mt-5 z-50 h-20 font-semibold opacity-95 bg-opacity-90 shadow-md backdrop-filter-[40px] bg-slate-600 fixed">
-        <ul className="flex justify-end mt-7 mr-16 text-center">
-          <img src="/nevtik.png" alt="" className="h-[62px] absolute left-0 ml-4 top-0 mt-2" />
+      <nav className="md:w-[40vw] mt-10   rounded-full md:mt-5 z-50 md:h-20 font-semibold opacity-95 bg-opacity-90 shadow-md backdrop-filter-[40px] bg-slate-600 fixed">
+        <ul className="flex justify-end md:mt-7 mr-16 text-center my-6 ">
+          <img src="/nevtik.png" alt="" className="md:h-[62px] md:w-16 w-10 absolute left-0 ml-4 top-0 mt-4 md:mt-[0.7rem]"  />
           {navbarItems.map((item, index) => (
             <li key={index} className="flex items-center justify-center relative w-28 h-6">
-              <Link href={item.link} className="flex items-center">
+              <Link href={item.link} className="flex items-center  ">
                 <img
                   onMouseEnter={() => setHoveredIndex(index)}
                   onMouseLeave={() => setHoveredIndex(null)}
                   src={hoveredIndex === index ? item.gif : item.static}
-                  className={`z-10 transition-transform duration-200 pl-2 ${
+                  className={`z-10 ml-9 md:ml-0 transition-transform duration-200 pl-2 ${
                     hoveredIndex === index
                       ? " h-[33.3px] relative -left-0 "
                       : "h-[25px]"
                   }`}
                   alt={item.label}
                 />
-                <span className={`px-10 absolute w-1 text-white ${hoveredIndex === index ? "left-10" : ""}`}>{item.label}</span>
+                <span className={`md:px-10 px-[4.5rem] absolute w-1 text-white ${hoveredIndex === index ? "left-10" : ""}`}>{item.label}</span>
               </Link>
             </li>
           ))}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import { CanvasContainer } from "./components/3D/Canvas/CanvasContainer";
+
 import HeroPage from "./pages/Hero/Hero";
 import FurniturePage from "./pages/Furniture/Furniture";
 import ProductPage from "./pages/Product/Product";
