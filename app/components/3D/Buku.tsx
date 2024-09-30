351593ce1846bd8e9145635f3b7051b90d428075
Claudyo pasqal <pasqaldyo@gmail.com>
2024-09-30 17:05:38 +0700

    create a route

--- app/components/3D/Buku.tsx
+++
@@ -1,9 +0,0 @@
-import { CanvasContainer } from "./Canvas/CanvasContainer";
-
-export default function Buku() {
-  return (
-    <div className="w-[10%] relative left-40 ">
-        <CanvasContainer />
-    </div>
-  )
-}
